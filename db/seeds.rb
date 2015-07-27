08945bc73f5954d20186e50b43fc1e14b2dd856b
Lianne <lyharrison@sky.com>
2015-07-27 19:39:26 +0000

    addded CRUD to deal with our restful routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create!(
+  title: "Ghost World",
+  director: "Terry Zwigoff",
+  runtime_in_minutes: 120,
+  description: "This is the story of Enid and Rebecca after they
+  finish the high school. Both have problems relating to people and
+  they spend their time hanging around and bothering creeps.
+  When they meet Seymour who is a social outsider who loves to collect
+  old 78 records, Enid's life will change forever",
+  poster_image_url: "https://en.wikipedia.org/wiki/Ghost_World_(film)",
+  release_date: "2001/07/20"
+)
