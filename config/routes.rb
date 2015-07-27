818fc6cf205e57e51ebea79ff5beb4f0fe960bda
Lianne <lyharrison@sky.com>
2015-07-27 22:30:25 +0000

    add login and logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ RottenMangos::Application.routes.draw do
   resources :movies
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
