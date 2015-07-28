b6fe1ea6028705f1ad27f539a7ce4a0137062c77
Lianne <lyharrison@sky.com>
2015-07-28 23:06:26 +0000

    admin management pages only accessible when an admin user is logged in

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727223623) do
+ActiveRecord::Schema.define(version: 20150728210620) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
     t.string   "director"
     t.integer  "runtime_in_minutes"
     t.text     "description"
-    t.string   "poster_image_url"
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
+    t.string   "poster_image_url"
   end
 
   create_table "reviews", force: true do |t|
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150727223623) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "is_admin",        default: false, null: false
   end
 
 end
